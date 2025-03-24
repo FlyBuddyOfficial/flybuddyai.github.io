6b01f00040543e9f9ddbecdd5a1790c1cd35e25f
FlyBuddyOfficial <flybuddy.ai@gmail.com>
2025-03-24 21:56:40 +0500

    Delete update-count.php

--- update-count.php
+++
@@ -1,6 +0,0 @@
-<?php
-$data = json_decode(file_get_contents('php://input'), true);
-if (isset($data['count'])) {
-    file_put_contents('join-count.json', json_encode(["count" => $data['count']]));
-}
-?>
